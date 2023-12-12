e369a4c711b6a5d2355f1c707bdf69df30314e82
gi-dor <rltjs3563@naver.com>
2023-12-12 12:03:48 +0900

    Feat :  JOIN _ SELF JOIN 추가

--- SQL/workspace/20231211_JOIN.sql
+++ SQL/workspace/20231211_JOIN.sql
@@ -20,26 +20,31 @@
 -- REGIONS 테이블과 COUNTRIES 테이블 JOIN
 -- REGIONS 테이블의 모든 행과 COUNTRIES 테이블의 모든행 JOIN
 select *
-from regions ,countries;
+from regions,
+     countries;
 
 -- REGIONS 테이블과 COUNTRIES 테이블 JOIN , 서로 연관있는 행만 필터링
-select c.country_id ,
+select c.country_id,
        c.country_name,
        r.region_name
-from regions r ,countries c
+from regions r,
+     countries c
 where r.region_id = c.region_id;
 
 -- 직원 아이디 , 직원 이름 , 직종아이디 , 직종최소급여 , 직종최대급여 , 급여조회하기
-select * from employees;
-select * from jobs;
+select *
+from employees;
+select *
+from jobs;
 
-select e.employee_id ,
-       e.first_name ,
+select e.employee_id,
+       e.first_name,
        e.job_id,
-       j.min_salary ,
-       j.max_salary ,
+       j.min_salary,
+       j.max_salary,
        e.salary
-from employees e , jobs j
+from employees e,
+     jobs j
 where e.job_id = j.job_id;
 
 
@@ -62,63 +67,84 @@ where e.job_id = j.job_id;
 */
 
 -- 직원아이디,직원이름, 소속부서아이디 , 소속부서명 조회
-select * from employees;
-select * from departments;
+select *
+from employees;
+select *
+from departments;
 
 -- Oracle의 등가조인
-select e.employee_id ,
-       e.first_name ,
-       e.department_id ,
+select e.employee_id,
+       e.first_name,
+       e.department_id,
        d.department_name
-from employees e , departments d
+from employees e,
+     departments d
 where e.department_id = d.department_id;
 
 
 --ANSI SQL의 등가조인
-select e.employee_id ,
-       e.first_name ,
-       e.department_id ,
+select e.employee_id,
+       e.first_name,
+       e.department_id,
        d.department_name
-from employees e JOIN departments d
-                      ON e.department_id = d.department_id;
+from employees e
+         JOIN departments d
+              ON e.department_id = d.department_id;
 
 
 -- 부서관리자가 지정된 부서의 부서아이디 , 부서명 , 부서관리자명 조회하기
 --                          D       D           D
 --                                              E
-select * from employees;
-select * from departments;
+select *
+from employees;
+select *
+from departments;
 
 -- Oracle 버전
-select d.department_id , d.department_name , e.first_name
-from employees e , departments d
+select d.department_id, d.department_name, e.first_name
+from employees e,
+     departments d
 where d.department_id = e.department_id;
 
 -- ANSI 버전
-select d.department_id , d.department_name , e.first_name
-from employees e JOIN departments d
-                      ON d.department_id = e.department_id
+select d.department_id, d.department_name, e.first_name
+from employees e
+         JOIN departments d
+              ON d.department_id = e.department_id
 where d.manager_id is not null;
 
 -- 직원의 아이디 이름  소속부서아이디 소속부서명 직종아이디 직종이름 급여 조회
 --      E       E       E                   E              E
 --              D                   D
 --                                          J       J
-select * from employees;
-select * from jobs;
-select * from  departments;
+select *
+from employees;
+select *
+from jobs;
+select *
+from departments;
 
-select e.employee_id , e.first_name ,
-       e.department_id , d.department_name ,
-       j.job_id, j.job_title , e.salary
-from employees e , jobs j , departments d
+select e.employee_id,
+       e.first_name,
+       e.department_id,
+       d.department_name,
+       j.job_id,
+       j.job_title,
+       e.salary
+from employees e,
+     jobs j,
+     departments d
 where e.job_id = j.job_id
   and e.department_id = d.department_id;
 
 -- ANSI 버전
-select e.employee_id , e.first_name ,
-       e.department_id , d.department_name ,
-       j.job_id, j.job_title , e.salary
+select e.employee_id,
+       e.first_name,
+       e.department_id,
+       d.department_name,
+       j.job_id,
+       j.job_title,
+       e.salary
 from employees e
          JOIN jobs j ON e.job_id = j.job_id
          JOIN departments d ON e.department_id = d.department_id;
@@ -129,35 +155,46 @@ from employees e
 --  E           E
 --                          L
 
-select * from employees;
-select * from locations;
-select * from departments;
+select *
+from employees;
+select *
+from locations;
+select *
+from departments;
 
-select e.employee_id , e.first_name , l.city
-from employees e ,departments d , locations l
+select e.employee_id, e.first_name, l.city
+from employees e,
+     departments d,
+     locations l
 where e.department_id = d.department_id
   and d.location_id = l.location_id;
-
 
 
 /*
 비등가조인용 샘플 테이블 추가
 */
 
-CREATE TABLE SALARY_GRADES (
-                               GRADE CHAR(1) PRIMARY KEY,
-                               MIN_SALARY NUMBER(8, 2),
-                               MAX_SALARY NUMBER(8, 2)
+CREATE TABLE SALARY_GRADES
+(
+    GRADE      CHAR(1) PRIMARY KEY,
+    MIN_SALARY NUMBER(8, 2),
+    MAX_SALARY NUMBER(8, 2)
 );
 
-INSERT INTO SALARY_GRADES VALUES('A', 0, 2499);
-INSERT INTO SALARY_GRADES VALUES('B', 2500, 4999);
-INSERT INTO SALARY_GRADES VALUES('C', 5000, 9999);
-INSERT INTO SALARY_GRADES VALUES('D', 10000, 19999);
-INSERT INTO SALARY_GRADES VALUES('E', 20000, 39999);
+INSERT INTO SALARY_GRADES
+VALUES ('A', 0, 2499);
+INSERT INTO SALARY_GRADES
+VALUES ('B', 2500, 4999);
+INSERT INTO SALARY_GRADES
+VALUES ('C', 5000, 9999);
+INSERT INTO SALARY_GRADES
+VALUES ('D', 10000, 19999);
+INSERT INTO SALARY_GRADES
+VALUES ('E', 20000, 39999);
 
 commit;
-select * from salary_grades;
+select *
+from salary_grades;
 
 
 /*  NON_EQUI JOIN
@@ -169,34 +206,37 @@ select * from salary_grades;
 -- 직원아이디 , 직원 이름    ,    급여   ,    급여 등급 조회
 --  E           E           E
 --                         G(최소/최대)       G
-select * from employees;
-select * from SALARY_GRADES;
+select *
+from employees;
+select *
+from SALARY_GRADES;
 
-select e.employee_id ,
-       e.first_name ,
+select e.employee_id,
+       e.first_name,
        e.salary,
        g.grade,
        g.min_salary,
        g.max_salary
-from employees e , salary_grades g;
+from employees e,
+     salary_grades g;
 
 
 
-select e.employee_id ,
-       e.first_name ,
-       e.salary ,
+select e.employee_id,
+       e.first_name,
+       e.salary,
        g.grade
-from employees e , SALARY_GRADES g
+from employees e,
+     SALARY_GRADES g
 where e.salary <= max_salary
   and e.salary >= min_salary
 order by e.employee_id;
 
 
-
 --ANSI SQL
-select e.employee_id ,
-       e.first_name ,
-       e.salary ,
+select e.employee_id,
+       e.first_name,
+       e.salary,
        g.grade
 from employees e
          JOIN salary_grades g
@@ -215,44 +255,51 @@ order by e.employee_id;
 -- 부서 아이디 , 부서이름 , 부서관리자 아이디 , 부서관리자 이름
 --      D       D       D - manager_id
 --                      E - employee_id           E - first_name
-select * from departments;
-select * from employees;
+select *
+from departments;
+select *
+from employees;
 
-select d.department_id , d.department_name , d.manager_id , e.first_name
-from employees e , departments d
+select d.department_id, d.department_name, d.manager_id, e.first_name
+from employees e,
+     departments d
 where e.employee_id(+) = d.manager_id
 order by d.department_id;
-
 
 
 
 -- 직원의 아이디 , 직원의 이름 , 소속부서 아이디 , 소속부서명 조회하기
 --  E               E       E
 --                          D               D
-select * from employees;
-select * from departments;
+select *
+from employees;
+select *
+from departments;
 
-select e.employee_id , e.first_name , e.department_id , d.department_name
-from employees e , departments d
+select e.employee_id, e.first_name, e.department_id, d.department_name
+from employees e,
+     departments d
 where e.department_id = d.department_id;
 -- 총107개인데 106개가 조회됨
 
-select * from employees
+select *
+from employees
 where employee_id = 178;
 -- 178번에 department_id 가 null
 
-select e.employee_id , e.first_name , e.department_id , d.department_name
-from employees e , departments d
+select e.employee_id, e.first_name, e.department_id, d.department_name
+from employees e,
+     departments d
 where e.department_id = d.department_id(+);
 
 
 -- ANSI 버전
-select e.employee_id ,
-       e.first_name ,
+select e.employee_id,
+       e.first_name,
        e.department_id,
        d.department_name
 from employees e
-         LEFT OUTER JOIN departments d   -- LEFT OUTER JOIN은 선행테이블의 모든 행을 조회한다
+         LEFT OUTER JOIN departments d -- LEFT OUTER JOIN은 선행테이블의 모든 행을 조회한다
                          ON e.department_id = d.department_id;
 
 
@@ -279,3 +326,49 @@ where A.column(+) = b.column(+);    FULL OUTER JOIN table2 B
 
 ** 오라클은 FULL OUTER JOIN 허용 안함
 */
+
+
+/*
+SELF JOIN
+
+자체 조인
+
+- JOIN 대상이 되는 테이블이 같은 테이블 일 때 발생 ㅡ 서로 연관지어서 조회할 때 사용
+- 하나의 테이블을 이용해서 JOIN구성 , 조회하는 컬럼명이 동일한 경우가 발생할 수 있음
+    + ✔ 별칭 적절히 사용
+- 서로 역할을 각각 나누어 별칭을 정하고 그 역할에 맞게 JOIN 조건 정의
+
+ */
+
+-- emplpyees 테이블에서 직원아이디 , 직원이름 , 해당직원의 상사아이디 , 상사 이름 조회
+
+select emp.employee_id as emp_id,
+       emp.first_name  as emp_name,
+       mgr.employee_id as mgr_id,
+       mgr.first_name  as mgr_name
+from employees emp,
+     employees mgr
+where emp.manager_id = mgr.employee_id;
+
+
+-- 직원 아이디, 직원 이름 , 직원의 소속부서명 , 상사의 아이디 , 상사의 이름 , 상사의 소속부서명
+--      E       E               E           E
+--                              D1                                      D2
+--                                           M          M
+select *
+from DEPARTMENTS;
+
+select emp.EMPLOYEE_ID    직원아이디,
+       emp.FIRST_NAME     직원이름,
+       d1.DEPARTMENT_NAME 직원의소속부서,
+       mgr.EMPLOYEE_ID    상사아이디,
+       mgr.FIRST_NAME     상사이름,
+       d2.DEPARTMENT_NAME 상사의부서
+from employees emp,
+     employees mgr,
+     departments d1,
+     departments d2
+where emp.MANAGER_ID = mgr.EMPLOYEE_ID(+)
+  and emp.department_id = d1.DEPARTMENT_ID (+)
+  and mgr.DEPARTMENT_ID = d2.DEPARTMENT_ID (+)
+order by emp.EMPLOYEE_ID;
